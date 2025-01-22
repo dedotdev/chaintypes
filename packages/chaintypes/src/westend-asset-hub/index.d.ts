9dad9a7b0c3a9de4420fa9f6052219cbe87477e9
debot <hi@coongcrafts.io>
2025-01-22 01:03:31 +0000

    chore: regenerate chaintypes

--- packages/chaintypes/src/westend-asset-hub/index.d.ts
+++ packages/chaintypes/src/westend-asset-hub/index.d.ts
@@ -23,7 +23,7 @@ export interface VersionedWestendAssetHubApi<Rv extends RpcVersion> extends Gene
 
 /**
  * @name: WestendAssetHubApi
- * @specVersion: 1017004
+ * @specVersion: 1017005
  **/
 export interface WestendAssetHubApi {
   legacy: VersionedWestendAssetHubApi<RpcLegacy>;

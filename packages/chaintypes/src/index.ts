1261681a431973a32cb8c74a20fca6af84576ff3
Thang X. Vu <thang@dedot.dev>
2025-05-19 21:59:27 +0700

    chore: generate chaintypes for new chains & expose ChainApi interface

--- packages/chaintypes/src/index.ts
+++ packages/chaintypes/src/index.ts
@@ -1,12 +1,24 @@
 export type { SubstrateApi } from './substrate/index.js';
+
 export type { PolkadotApi } from './polkadot/index.js';
-export type { KusamaApi } from './kusama/index.js';
-export type { MoonbeamApi } from './moonbeam/index.js';
-export type { AstarApi } from './astar/index.js';
-export type { KusamaAssetHubApi } from './kusama-asset-hub/index.js';
-export type { AlephApi } from './aleph/index.js';
 export type { PolkadotAssetHubApi } from './polkadot-asset-hub/index.js';
+export type { PolkadotPeopleApi } from './polkadot-people/index.js';
+
+export type { KusamaApi } from './kusama/index.js';
+export type { KusamaAssetHubApi } from './kusama-asset-hub/index.js';
+export type { KusamaPeopleApi } from './kusama-people/index.js';
+
 export type { WestendApi } from './westend/index.js';
 export type { WestendPeopleApi } from './westend-people/index.js';
 export type { WestendAssetHubApi } from './westend-asset-hub/index.js';
+
 export type { PaseoApi } from './paseo/index.js';
+export type { PaseoAssetHubApi } from './paseo-asset-hub/index.js';
+export type { PaseoPeopleApi } from './paseo-people/index.js';
+export type { PaseoHydrationApi } from './paseo-hydration/index.js';
+
+export type { HydrationApi } from './hydration/index.js';
+export type { BasiliskApi } from './basilisk/index.js';
+export type { MoonbeamApi } from './moonbeam/index.js';
+export type { AstarApi } from './astar/index.js';
+export type { AlephApi } from './aleph/index.js';

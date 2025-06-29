55c56fa8b9303cdf86a18d9ba6e7502d7c367b50
debot <hi@coongcrafts.io>
2025-06-29 01:24:21 +0000

    chore: regenerate chaintypes

--- packages/chaintypes/src/moonbeam/index.d.ts
+++ packages/chaintypes/src/moonbeam/index.d.ts
@@ -23,7 +23,7 @@ export interface VersionedMoonbeamApi<Rv extends RpcVersion> extends GenericSubs
 
 /**
  * @name: MoonbeamApi
- * @specVersion: 3701
+ * @specVersion: 3702
  **/
 export interface MoonbeamApi {
   legacy: VersionedMoonbeamApi<RpcLegacy>;
